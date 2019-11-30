aca64a838a22a216033dadd3b24aa6b746416838
kakao0228 <54926671+kakao0228@users.noreply.github.com>
2019-11-30 23:08:49 +0900

    test picture add

--- navbar.js
+++ navbar.js
@@ -1,12 +1,11 @@
 "use strict"
 
-function hovermenu() {
-  $('.hovermenu').mouseover(function() {
-    $('.hovermeun').find('.invisible').addclass('visible')
-    $('.hovermeun').find('.invisible').removeclass('invisible');
+$(document).ready(function() {
+  $("ul.menu > li").on('mouseover', function(){
+    $(this).find('a').addClass('hover');
+    $(this).find('ul.sub-list').show();
+  }).on('mouseout',function(){
+    $(this).find('a').removeClass('hover');
+    $(this).find('ul.sub-list').hide();
   });
-  $('.hovermenu').mouseout(function() {
-    $('.hovermeun').find('.invisible').addclass('visible')
-    $('.hovermeun').find('.invisible').removeclass('invisible');
-  });
-}
+});
