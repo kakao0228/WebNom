6ea6235841036f4751134e59301d105ac0d001a1
kakao0228 <54926671+kakao0228@users.noreply.github.com>
2019-12-01 00:46:26 +0900

    hover

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,10 @@
 "use strict"
 
 $(document).ready(function() {
-  $("ul.menu > li").on('mouseover', function(){
-    $(this).find('a').addClass('hover');
-    $(this).find('ul.sub-list').show();
-  }).on('mouseout',function(){
-    $(this).find('a').removeClass('hover');
-    $(this).find('ul.sub-list').hide();
+  $('.menu li first:a').on('mouseover', function() {
+    $('.sub-list').slidedown(200);
+  });
+  $('.menu li first:a').on('mouseout', function() {
+    $('.sub-list').slideup(200);
   });
 });
